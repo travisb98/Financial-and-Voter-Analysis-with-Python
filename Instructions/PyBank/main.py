26ba52f7d41e3da758d7d8d47f96d361218730e5
unknown <travisbostick@gmail.com>
2020-10-06 19:59:26 -0500

    added some notes about using an array to calculate the average change instead of trying to pull the previous iteration from a for loop

--- Instructions/PyBank/main.py
+++ Instructions/PyBank/main.py
@@ -17,11 +17,12 @@ with open(csvpath) as budgetfile:
         totalprofit = totalprofit + int(row[1])      
         #finds the total number of rows which will be used to determine the number of months
         months += 1
-        #testing out previous rows, trying to calculate average change
-        currentprofit = 0
-        nextmonthsprofit = 0
-        currentprofit = row[1]
-        nextmonthsprofit = # i need to be able to reference the vaule in column 2 for the next row
+        # #to find the average change, I should create an array of each month's change, appending each iteration to the array to create it, then find the average of the array
+        # ...my method below should be scrapped
+        # currentprofit = 0
+        # nextmonthsprofit = 0
+        # currentprofit = row[1]
+        # nextmonthsprofit = # i need to be able to reference the vaule in column 2 for the next row
 
 
 #filler for now so the values I get are printed to the terminal
