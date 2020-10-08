4a2d848ac10ad4376027524ef9fd899be2918b05
unknown <travisbostick@gmail.com>
2020-10-08 17:47:00 -0500

    Completed PyBank

--- Instructions/PyBank/main.py
+++ Instructions/PyBank/main.py
@@ -1,12 +1,30 @@
 import os
 import csv
-#error message for directory error
+#defining variables and functions
+
+###error message for directory error
 def errormsg():
     print("-----------------------------------------------------")
     print("-----------------------------------------------------")
     print("encountered FileNotFoundError, assesing directory")
     print("-----------------------------------------------------")
     print("-----------------------------------------------------")
+
+### defining the final results as a function so it can be printed to the terminal and the text document
+def final(x):
+    breaky = "\n"
+    x(f'{breaky}Financial Analysis{breaky}-------------------{breaky}Total Months: ${months}{breaky}Total Profit: ${totalprofit}{breaky}Average Change: ${averagechange}{breaky}Greatest Increase {winnermonth} (${gincrease}){breaky}Greatest Decrease: {losermonth} (${gdecrease})')
+
+#### setting arrays and variables
+totalprofit = 0
+months = 0
+currentprofit = 0
+prevmonthprofit = 0
+currentchange = 0
+totalchange = 0
+gincrease = 0
+gdecrease = 0
+
 
 #testing out using try blocks to deal with error handling regarding directory names, i should iterate this to check for each file within the project's structure.
 try:
@@ -21,28 +39,16 @@ except FileNotFoundError:
         errormsg
         csvpath = os.path.join("Instructions","PyBank","Resources","budget_data.csv")
 
-
 #open the CSV file
-
 with open(csvpath) as budgetfile:
     filereader = csv.reader(budgetfile,delimiter=",")
 
     #skips the header
     next(filereader)
     
-    # setting arrays and variables
-    totalprofit = 0
-    months = 0
-    currentprofit = 0
-    prevmonthprofit = 0
-    currentchange = 0
-    totalchange = 0
-    gincrease = 0
-    gdecrease = 0
-
     #reads the file
     for row in filereader:
-        #calculates the total profit, commented out because I'm using the array to do this
+        #calculates the total profit
         totalprofit = totalprofit + int(row[1])      
         #finds the total number of rows which will be used to determine the number of months
         months += 1
@@ -61,10 +67,6 @@ with open(csvpath) as budgetfile:
         prevmonthprofit = int(row[1])
     #calculates the average change   
     averagechange = round(totalchange/(months-1),2)
-# defining the final results as a function so it can be printed to the terminal and the text document
-def final(x):
-    breaky = "\n"
-    x(f'{breaky}Financial Analysis{breaky}-------------------{breaky}Total Months: ${months}{breaky}Total Profit: ${totalprofit}{breaky}Average Change: ${averagechange}{breaky}Greatest Increase {winnermonth} (${gincrease}){breaky}Greatest Decrease: {losermonth} (${gdecrease})')
 
 #prints the results to the terminal
 final(print)
@@ -82,6 +84,6 @@ except FileNotFoundError:
         errormsg
         resultspath = os.path.join("Instructions","PyBank","analysis","PYBANK_results.txt")
         open(resultspath,'w+')
-
+#creates PYBANK_results file an writes the final results to the file
 with open(resultspath,'w+') as resultsfile:
     final(resultsfile.write)
